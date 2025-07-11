45179dcccf1808d253905cc746d568dd9711dae7
Francisco <frajaroco@gmail.com>
2025-07-11 17:41:49 -0500

    Commit 02-110725

--- R/boxcountdimTest.R
+++ R/boxcountdimTest.R
@@ -1,25 +1,20 @@
 # Function log-log test
-boxcountdimTest <- function(X ,nsimev=NULL){
+boxcountdimTest <- function(X, nsimev=100, alpha=0.05){
 
 # X is a lpp object
 verifyclass(X,"lpp") 
 BCD <- box_count_dim(X)
 
-if(is.null(nsimev)) {
-  tsim=100
-} else {
-  tsim=nsimev}
-
-D_sim <- box_count_dim_pois(X, ns = tsim)$D
+D_sim <- box_count_dim_pois(X, ns = nsimev)$D
 B_calc <- BCD$D_theo-BCD$D_est
-q1=quantile(D_sim, 0.025)
-q3=quantile(D_sim, 0.975)
+q1=quantile(D_sim, alpha/2)
+q3=quantile(D_sim, 1-alpha/2)
 
 ifelse(B_calc < median(D_sim),
-       ValorP <- 2*sum(unlist(lapply(1:tsim, function(x){
-         ifelse(D_sim[x] < B_calc, 1, 0)})))/tsim,
-       ValorP <- 2*sum(unlist(lapply(1:tsim, function(x){
-         ifelse(D_sim[x] > B_calc, 1, 0)})))/tsim)
+       ValorP <- 2*sum(unlist(lapply(1:nsimev, function(x){
+         ifelse(D_sim[x] < B_calc, 1, 0)})))/nsimev,
+       ValorP <- 2*sum(unlist(lapply(1:nsimev, function(x){
+         ifelse(D_sim[x] > B_calc, 1, 0)})))/nsimev)
 
 invisible(return(list(Q1=q1, Q3=q3, B_calc = B_calc, PV=ValorP, B_sim=D_sim)))
 }
